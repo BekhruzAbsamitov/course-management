14b81ed9ff83e84eb282ee0f00d3bc38d7d15afa
Diorgoth <supremediyorbek4013@gmail.com>
2021-03-18 22:16:39 +0500

    Entities with db connections

---
+++ src/main/java/com/example/demo/Entity/Region.java
@@ -0,0 +1,24 @@
+package com.example.demo.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+
+
+}
