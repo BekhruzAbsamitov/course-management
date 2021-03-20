a43846dc18b30d9d45d06cf60d27587a15d29b4b
zukhranazarova <nazarovazuhra356@gmail.com>
2021-03-20 21:40:57 +0500

    commit for course controller and active course controller

--- src/main/java/com/example/demo/controller/ApplicationController.java
+++ src/main/java/com/example/demo/controller/ApplicationController.java
@@ -1,14 +1,45 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Application;
+import com.example.demo.payload.ApplicationDto;
+import com.example.demo.payload.Result;
 import com.example.demo.service.ApplicationService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/application")
 public class ApplicationController {
 
     ApplicationService applicationService;
 
     public ApplicationController(ApplicationService applicationService) {
         this.applicationService = applicationService;
+    }
+
+    @DeleteMapping("/{id}")
+    public Result deleteApplication(@PathVariable Integer id) {
+        return applicationService.deleteApplication(id);
+    }
+
+    @PutMapping("/{id}")
+    public Result editApplication(@PathVariable Integer id, @RequestBody ApplicationDto applicationDto) {
+        return applicationService.editApplication(id, applicationDto);
+    }
+
+    @PostMapping
+    public Result addApplication(ApplicationDto applicationDto) {
+        return applicationService.addApplication(applicationDto);
+    }
+
+    @GetMapping
+    public List<Application> getApplicationList() {
+        return applicationService.getApplicationList();
+    }
+
+    @GetMapping("/{id}")
+    public Application getApplication(@PathVariable Integer id) {
+        return applicationService.getApplicationById(id);
     }
 }
--- src/main/java/com/example/demo/service/ApplicationService.java
+++ src/main/java/com/example/demo/service/ApplicationService.java
@@ -1,8 +1,13 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.ActiveCourse;
 import com.example.demo.entity.Application;
+import com.example.demo.entity.User;
 import com.example.demo.payload.ApplicationDto;
+import com.example.demo.payload.Result;
+import com.example.demo.repository.ActiveCourseRepository;
 import com.example.demo.repository.ApplicationRepository;
+import com.example.demo.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,20 +17,86 @@ import java.util.Optional;
 public class ApplicationService {
 
     ApplicationRepository applicationRepository;
+    UserRepository userRepository;
+    ActiveCourseRepository activeCourseRepository;
 
-    public ApplicationService(ApplicationRepository applicationRepository) {
+
+    public ApplicationService(ApplicationRepository applicationRepository, UserRepository userRepository, ActiveCourseRepository activeCourseRepository) {
         this.applicationRepository = applicationRepository;
+        this.userRepository = userRepository;
+        this.activeCourseRepository = activeCourseRepository;
     }
 
-    public String addApplication(ApplicationDto applicationDto) {
+    public Result editApplication(Integer id, ApplicationDto applicationDto) {
+        final Optional<Application> optionalApplication = applicationRepository.findById(id);
+        if (!optionalApplication.isPresent()) {
+            return new Result("Application not found", false);
+        }
+        final Application application = optionalApplication.get();
+
+        application.setMessage(applicationDto.getMessage());
+        application.setStatus(applicationDto.getStatus());
+
+        final Optional<User> repository = userRepository.findById(applicationDto.getUserId());
+        if (!repository.isPresent()) {
+            return new Result("User id not found", false);
+        }
+        final Optional<ActiveCourse> activeCourse = activeCourseRepository.findById(applicationDto.getActiveCourseId());
+        if (!activeCourse.isPresent()) {
+            return new Result("Active Course id not found", false);
+        }
+        final boolean course = applicationRepository.existsByUserAndActiveCourse(repository.get(), activeCourse.get());
+        if (course) {
+            return new Result("User already in active course", false);
+        }
+        application.setActiveCourse(activeCourse.get());
+        application.setUser(repository.get());
+
+        try {
+            applicationRepository.save(application);
+        } catch (Exception e) {
+            return new Result("Error in editing", false);
+        }
+        return new Result("Application edited!", true);
+    }
+
+    public Result deleteApplication(Integer id) {
+        final boolean isExists = applicationRepository.existsById(id);
+
+        if (isExists) {
+            applicationRepository.deleteById(id);
+            return new Result("Application deleted", true);
+        }
+        return new Result("Application not found", false);
+    }
+
+    public Result addApplication(ApplicationDto applicationDto) {
         Application application = new Application();
 
         application.setMessage(applicationDto.getMessage());
         application.setStatus(applicationDto.getStatus());
 
+        final Optional<User> repository = userRepository.findById(applicationDto.getUserId());
+        if (!repository.isPresent()) {
+            return new Result("User id not found", false);
+        }
+        final Optional<ActiveCourse> activeCourse = activeCourseRepository.findById(applicationDto.getActiveCourseId());
+        if (!activeCourse.isPresent()) {
+            return new Result("Active Course id not found", false);
+        }
+        final boolean course = applicationRepository.existsByUserAndActiveCourse(repository.get(), activeCourse.get());
+        if (course) {
+            return new Result("User already in active course", false);
+        }
+        application.setActiveCourse(activeCourse.get());
+        application.setUser(repository.get());
 
-//        applicationRepository.existsByUserAndActiveCourse();
-        return null;
+        try {
+            applicationRepository.save(application);
+        } catch (Exception e) {
+            return new Result("Error in adding", false);
+        }
+        return new Result("Application added!", true);
     }
 
     public List<Application> getApplicationList() {
